68c59af050ddc2189fc0ac7bef76af7c1559599e
Abdul Elrakh <abdulelrakh@gmail.com>
2024-12-27 04:05:36 +0100

    First Project Problem 02

---
+++ 3DPoint.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_4
+{
+    internal class _3DPoint : IComparable,ICloneable
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public _3DPoint() : this(0,0,0)
+        {
+
+        }
+
+        public _3DPoint(double X, double Y) : this(X , Y, 0)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+
+        public _3DPoint(double _X, double _Y, double _Z) 
+        {
+            this.X = _X;
+            this.Y = _Y;
+            this.Z = _Z;
+        }
+
+
+        public int CompareTo(object? point)
+        {
+            _3DPoint p2 = (_3DPoint)point;
+
+
+            if (this.X == p2.X && this.Y == p2.Y && this.Z == p2.Z)
+            {
+                return 5;
+            }
+
+            double dist = Math.Sqrt(Math.Pow(this.X, 2) + Math.Pow(this.Y, 2));
+            double dist2 = Math.Sqrt(Math.Pow(p2.X, 2) + Math.Pow(p2.Y, 2));
+
+
+
+            if (dist > dist2)
+            {
+                return 1;
+            }
+            if (dist < dist2) { return -1; }
+
+
+            return 0;
+
+        }
+
+        public object Clone()
+        {
+            return new _3DPoint(this.X, this.Y, this.Z);
+        }
+
+
+
+        override
+
+        public string ToString()
+        {
+            return $"Point Coordinates: ({this.X}, {this.Y}, {this.Z})";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ using OOP_4;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using System.Threading.Channels;
 using System;
+using System.Drawing;
+using System.Runtime.Intrinsics.X86;
 
 namespace OOP4
 {
@@ -56,9 +58,200 @@ namespace OOP4
             //push.SendNotification("Abdelrahman", "Where are you");
 
 
-            #endregion      
+            #endregion
+
+
+            #region Part 02 First Project
+            ///Define 3D Point Class and the basic Constructors(use chaining in constructors).
+            ///Override the ToString Function to produce this output:
+            ///            Point3D P = new Point3D(10, 10, 10);
+            ///            Console.WriteLine(P.ToString());
+            ///        Output: “Point Coordinates: (10, 10, 10)”.
+            /// Read from the User the Coordinates for 2 points P1, P2(Check the input using try Pares, Parse, Convert).
+
+            ///Try to use ==
+            ///If(P1 == P2)   Does it work properly? 
+            ///Define an array of points and sort this array based on X &Y coordinates.
+            ///Implement ICloneable interface to be able to clone the object.
+            ///To implement more than one interface.
+            ///class Point3D : IComparable, ICloneable 
+
+            //_3DPoint P = new _3DPoint(10, 10, 10);
+            //Console.WriteLine(P.ToString());
+            //int exit = 0;
+
+            //int _x = 0, _y = 0, _z = 0, _x2 = 0, _y2 = 0, _z2 = 0, _x3 = 0, _y3 = 0, _z3 = 0;
+            //do
+            //{
+            //    #region First POINT
+            //    Console.WriteLine("Insert the X coordinate for the first point");
+
+            //    bool x_input = int.TryParse(Console.ReadLine(), out int x);
+            //    if (!x_input)
+            //    {
+            //        break;
+            //    }
+            //    _x = x;
+            //    Console.Clear();
+
+            //    Console.WriteLine("Insert the Y coordinate for the first point");
+
+            //    bool y_input = int.TryParse(Console.ReadLine(), out int y);
+            //    if (!y_input)
+            //    {
+            //        break;
+            //    }
+            //    _y = y;
+
+            //    Console.Clear();
+
+            //    Console.WriteLine("Insert the Z coordinate for the first point");
+
+            //    bool z_input = int.TryParse(Console.ReadLine(), out int z);
+            //    if (!z_input)
+            //    {
+            //        break;
+            //    }
+            //    _z = z;
+
+            //    Console.Clear();
+            //    #endregion
+
+            //    #region SECOND POINT
+            //    Console.WriteLine("Insert the X coordinate for the second point");
+
+            //    bool x2_input = int.TryParse(Console.ReadLine(), out int x2);
+            //    if (!x2_input)
+            //    {
+            //        break;
+            //    }
+            //    _x2 = x2;
+
+            //    Console.Clear();
+
+            //    Console.WriteLine("Insert the Y coordinate for the second point");
+
+            //    bool y2_input = int.TryParse(Console.ReadLine(), out int y2);
+            //    if (!y2_input)
+            //    {
+            //        break;
+            //    }
+            //    _y2 = y2;
+
+            //    Console.Clear();
+
+            //    Console.WriteLine("Insert the Z coordinate for the second point");
+
+            //    bool z2_input = int.TryParse(Console.ReadLine(), out int z2);
+            //    if (!z2_input)
+            //    {
+            //        break;
+            //    }
+            //    _z2 = z2;
+
+            //    Console.Clear();
+            //    #endregion
+
+            //    #region Third Point
+            //    Console.WriteLine("Insert the X coordinate for the third point");
+
+            //    bool x3_input = int.TryParse(Console.ReadLine(), out int x3);
+            //    if (!x3_input)
+            //    {
+            //        break;
+            //    }
+            //    _x3 = x3;
+
+            //    Console.Clear();
+
+            //    Console.WriteLine("Insert the Y coordinate for the third point");
+
+            //    bool y3_input = int.TryParse(Console.ReadLine(), out int y3);
+            //    if (!y_input)
+            //    {
+            //        break;
+            //    }
+            //    _y3 = y3;
+
+            //    Console.Clear();
+
+            //    Console.WriteLine("Insert the Z coordinate for the third point");
+
+            //    bool z3_input = int.TryParse(Console.ReadLine(), out int z3);
+            //    if (!z_input)
+            //    {
+            //        break;
+            //    }
+            //    _z3 = z3;
+
+            //    Console.Clear();
+            //    #endregion
+
+                
+
+            //    exit = 1;
+            //} while (exit != 1);
+
+
+            //_3DPoint P1 = new _3DPoint(_x, _y, _z);
+
+
+            //_3DPoint P2 = new _3DPoint(_x2,_y2, _z2);
+            //_3DPoint P3 = new _3DPoint(_x3,_y3, _z3);
+
+
+            //if (P1.CompareTo(P2) == 5)
+            //{
+            //    Console.WriteLine("This is an example of how Icomparable Works");
+            //}
+
+
+            //_3DPoint[] points = { P1, P2, P3 };
+            //_3DPoint temp = new _3DPoint();
+
+            //for (int i = 0; i < points.Length; i++) 
+            //{
+            //    for (int j = 1; j < points.Length; j++) 
+            //    {
+            //        if (points[i].CompareTo(points[j]) == 1)
+            //        {
+            //            temp = points[i];
+            //            points[i] = points[j];  
+            //            points[j] = temp;
+            //        }
+            //    }
+
+            //}
+
+            //Console.WriteLine("Sorted Array");
+
+            //for (int i = 0; i < points.Length; i++) 
+            //{
+            //    Console.WriteLine(points[i].ToString());
+            //}
+
+            // CLONE INTERFACE
+
+            //_3DPoint P1Clone = (_3DPoint) P1.Clone();
+
+            //Console.WriteLine(P1.ToString() + P1.GetHashCode());
+
+            //Console.WriteLine(P1Clone.ToString() + P1Clone.GetHashCode());
+
+
+
+
+
+
+
+
+
+
+            #endregion
+
+
 
 
         }
-}
+    }
 }
